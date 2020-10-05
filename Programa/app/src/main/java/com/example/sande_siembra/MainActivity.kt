e6dffd7c10aa28dac6c5afa1553210dd3e4ec37c
Jonathan <jonathan.alarcon@epn.edu.ec>
2020-10-05 07:41:05 -0500

    Cambios

--- Programa/app/src/main/java/com/example/sande_siembra/MainActivity.kt
+++ Programa/app/src/main/java/com/example/sande_siembra/MainActivity.kt
@@ -232,7 +232,7 @@ class MainActivity : AppCompatActivity() {
             )
         )*/
 
-        db.collection("SiembraPrueba").add(
+        db.collection("SiembraPrueba1").add(
             hashMapOf("Fecha" to currentDate,
                 "Semana" to numeroSemana, "Finca" to fincaNombre, "Valvula" to valvula,
                 "Bloque" to bloque,
