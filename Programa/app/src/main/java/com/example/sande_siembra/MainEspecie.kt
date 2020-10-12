2b267c280a4be1b6c67a065e20a7aaea68ba43b2
marianita <marianita.estrella@outlook.com>
2020-10-12 09:07:08 -0500

    hasta crear archivo csv

--- Programa/app/src/main/java/com/example/sande_siembra/MainEspecie.kt
+++ Programa/app/src/main/java/com/example/sande_siembra/MainEspecie.kt
@@ -1,15 +1,21 @@
 package com.example.sande_siembra
 
+import android.Manifest
 import android.app.Service
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_main.*
+import android.content.pm.PackageManager
 import android.net.ConnectivityManager
 import android.net.NetworkInfo
+import android.os.Bundle
+import android.os.Environment
 import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main_especie.*
+import java.io.File
+import java.io.FileWriter
 
 
 class MainEspecie : AppCompatActivity() {
@@ -29,6 +35,7 @@ class MainEspecie : AppCompatActivity() {
                 Log.i("error", "No se tiene conexion a internet")
             }
         }
+        pedirPermisos()
         btnLirios.setOnClickListener { Lirios() }
         btnFlorVerano.setOnClickListener { FlorVerano() }
 
@@ -60,11 +67,12 @@ class MainEspecie : AppCompatActivity() {
     }
     fun Lirios(){
 
+
+        exportarCSV()
         val intentExplicito = Intent(this, MenuInicio::class.java)
         val datolirio = "LIRIOS"
         intentExplicito.putExtra("especie", datolirio )
         startActivity(intentExplicito)
-
 
     }
 
@@ -77,6 +85,71 @@ class MainEspecie : AppCompatActivity() {
 
 
     }
+
+    fun pedirPermisos() {
+        // PERMISOS PARA ANDROID 6 O SUPERIOR
+        if (ContextCompat.checkSelfPermission(
+                this@MainEspecie,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this@MainEspecie,
+                arrayOf(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ),
+                0
+            )
+        }
+    }
+
+
+    fun exportarCSV() {
+        val carpeta = File(
+            Environment.getExternalStorageDirectory().toString() + "/ExportarSQLiteCSV"
+        )
+        val archivoAgenda = "$carpeta/Usuarios.csv"
+        var isCreate = false
+        if (!carpeta.exists()) {
+            isCreate = carpeta.mkdir()
+        }
+        try {
+            val fileWriter = FileWriter(archivoAgenda)
+            fileWriter.append("prueba1")
+            fileWriter.append(",")
+            fileWriter.append("prueba2")
+            /*val admin = AdminSQLiteOpenHelper(this@MainActivity, "dbSistema", null, 1)
+            val db: SQLiteDatabase = admin.getWritableDatabase()
+            val fila = db.rawQuery("select * from usuarios", null)
+            if (fila != null && fila.count != 0) {
+                fila.moveToFirst()
+                do {
+                    fileWriter.append(fila.getString(0))
+                    fileWriter.append(",")
+                    fileWriter.append(fila.getString(1))
+                    fileWriter.append(",")
+                    fileWriter.append(fila.getString(2))
+                    fileWriter.append("\n")
+                } while (fila.moveToNext())
+            } else {
+                Toast.makeText(this@MainActivity, "No hay registros.", Toast.LENGTH_LONG).show()
+            }
+            db.close()*/
+            fileWriter.close()
+            Toast.makeText(
+                this@MainEspecie,
+                "SE CREO EL ARCHIVO CSV EXITOSAMENTE",
+                Toast.LENGTH_LONG
+            ).show()
+        } catch (e: Exception) {
+        }
+    }
+
+
+
+
 
 
 }
