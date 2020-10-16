a7293a4374580b9be311edd61804cf09c0bbf095
Jonathan <jonathan.alarcon@epn.edu.ec>
2020-10-16 10:31:39 -0500

    Hasta pantalla de visualizar datos para edición (completa)

--- Programa/app/src/main/java/com/example/sande_siembra/MainAdapter.kt
+++ Programa/app/src/main/java/com/example/sande_siembra/MainAdapter.kt
@@ -18,7 +18,7 @@ class MainAdapter (private val context:Context): RecyclerView.Adapter<MainAdapte
 
     inner class MainViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
         fun bindView(siembra: Siembra){
-            itemView.txt_val.text = "Valvula: ${siembra.valvula.toString()}"
+            //itemView.txt_val.text = "Valvula: ${siembra.valvula.toString()}"
             itemView.txt_blo.text = "Bloque: ${siembra.bloque.toString()}"
         }
     }
